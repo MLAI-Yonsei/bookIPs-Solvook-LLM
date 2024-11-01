7050c574400f19a871611de2ee6843d06fd1d3c2
Choi-jun9803 <choiyj9803@gmail.com>
2024-11-01 15:36:34 +0900

    tiny fix

--- finetune.sh
+++ finetune.sh
@@ -1,3 +1,3 @@
-llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
+# llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
 # for multi gpu
-# CUDA_VISIBLE_DEVICES=1,2,3 llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
+CUDA_VISIBLE_DEVICES=1,2,3 llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
--- inference.sh
+++ inference.sh
@@ -1,3 +1,3 @@
-llamafactory-cli train examples/train_lora/llama3_lora_predict.yaml
+# NCCL_P2P_DISABLE="1" NCCL_IB_DISABLE="1" llamafactory-cli train examples/train_lora/llama3_lora_predict.yaml
 # for multi gpu
-# CUDA_VISIBLE_DEVICES=1,2,3 llamafactory-cli train examples/train_lora/llama3_lora_predict.yaml
+NCCL_P2P_DISABLE="1" NCCL_IB_DISABLE="1" CUDA_VISIBLE_DEVICES=1,2,3 llamafactory-cli train examples/train_lora/llama3_lora_predict.yaml
